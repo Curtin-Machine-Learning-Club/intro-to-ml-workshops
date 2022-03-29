71f32519a2a73bb2f6dbabdd9d8f1b21b77fb708
Harrison-O <mr.outram@protonmail.com>
2022-03-30 03:49:10 +0800

    Wrote non-linearly separable dat

--- src/classification/svm.py
+++ src/classification/svm.py
@@ -76,9 +76,52 @@ class SupportVectorMachineLinearPlot(Scene):
         self.play(Create(self.upper_hyperline), Create(self.lower_hyperplane))
         self.wait()
 
+class NonLinearlySeparableData(Scene):
+    def __init__(self):
+        super().__init__()
+        points1 = circularData(20, 3, 5)
+        points2 = circularData(20, 0.2, 2.5)
+
+        self.x1 = roundDownNearest(min(points2[:,0]), 1)
+        self.x2 = roundUpNearest(max(points2[:,0]), 1)
+        self.y1 = roundDownNearest(min(points2[:,1]), 1)
+        self.y2 = roundUpNearest(max(points2[:,1]), 1)
+        self.ax = Axes(
+            x_range=[self.x1, self.x2, 1], y_range=[self.y1, self.y2, 1], tips=False,
+            x_axis_config={
+                "numbers_to_include": np.arange(self.x1, self.x2, 1)
+            },
+            y_axis_config={
+                "numbers_to_include": np.arange(self.y1, self.y2, 1)
+            }
+        )
+        self.labels = self.ax.get_axis_labels(x_label=MathTex("x_1"), y_label=MathTex("x_2"))
+        dots = []
+
+        for pt1, pt2 in zip(points1, points2):
+            dot1 = Dot([self.ax.coords_to_point(pt1[0], pt1[1])], color=GREEN_C)
+            dot2 = Dot([self.ax.coords_to_point(pt2[0], pt2[1])], color=RED_C)
+            dots.extend([dot1, dot2])
+        
+        self.dots = VGroup(*dots)
+    
+    def construct(self):
+        self.wait()
+        self.play(Create(self.ax), Create(self.labels))
+        self.wait()
+        self.play(Create(self.dots))
+        self.wait()
+
 def roundDownNearest(num: float, div: int) -> int:
     return (num // div) * div
 
 def roundUpNearest(num: float, div: int) -> int:
     a = (num // div) * div
     return a + div
+
+def circularData(n: int, r1: float, r2: float) -> np.ndarray:
+    mags = np.random.random(n) * (r2 - r1)
+    X = mags * np.cos(2 * np.pi * np.random.random(n))
+    mags = np.random.random(n) * (r2 - r1)
+    Y = mags * np.sin(2 * np.pi * np.random.random(n))
+    return np.concatenate((X, Y), axis=1)
