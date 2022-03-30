336583cffa8e3df4a47b36d9f0be2c043e750dce
Harrison-O <mr.outram995@gmail.com>
2022-03-30 12:01:34 +0800

    Fixed logistic regression animations

--- src/classification/logistic_regression.py
+++ src/classification/logistic_regression.py
@@ -18,12 +18,12 @@ class LogisticRegression(Scene):
         self.y1 = 0
         self.y2 = 1.2
         self.ax = Axes(
-            x_range=[self.x1, self.x2, 0.2], y_range=[self.y1, self.y2, 0.2], tips=False,
+            x_range=[self.x1, self.x2, 1.0], y_range=[self.y1, self.y2, 0.5], tips=False,
             x_axis_config={
-                "numbers_to_include": np.arange(self.x1, self.x2, 0.2)
+                "numbers_to_include": np.arange(self.x1, self.x2, 1.0)
             },
             y_axis_config={
-                "numbers_to_include": np.arange(self.y1, self.y2, 0.2)
+                "numbers_to_include": np.arange(self.y1, self.y2, 0.5)
             }
         )
         self.labels = self.ax.get_axis_labels(x_label=MathTex('x'), y_label=MathTex('y'))
@@ -43,10 +43,10 @@ class LogisticRegressionEquations(Scene):
         super().__init__()
         self.sigmoidEq = MathTex(r'\hat{y} = \frac{1}{1 + e^{-(xw + b)}}').shift(UP * 2)
         self.sigmoidCost = MathTex(
-            r'C(\hat{y}, y) &= -\log(\hat{y}), \text{if} y=1 \\'
-            r'C(\hat{y}, y) &= -\log(1 - \hat{y}), \text{if} y=0 \\'
+            r'C(\hat{y}, y) &= -\log(\hat{y}), \text{if } y=1 \\',
+            r'C(\hat{y}, y) &= -\log(1 - \hat{y}), \text{if } y=0 \\',
             r'C(\hat{y}, y) &= -y\log(\hat{y}) - (1 - y) \log(1 - \hat{y})'
-        )
+        ).shift(DOWN)
     
     def construct(self):
         self.wait()
